cc81797199cdb202f67721a2ab155288681d7012
YouML <95748365+ChongyaSong@users.noreply.github.com>
2022-02-05 00:11:50 +0800

    Update setup_pip.py

--- setup_pip.py
+++ setup_pip.py
@@ -12,7 +12,7 @@ setup(
     version = '0.6',
     description = 'A Machine Learning Toolkit',
     long_description = 'YouML stands for “Your (free) Machine Learning (toolkit).” It intends to provide the machine learning community a free and no-code toolkit for preprocessing data and building machine learning models. Several key features will be released after no major bug can be found in the current version. The ultimate goal is to deliver a platform where users can obtain solutions to address tough problems in their machine learning tasks.',
-    platforms = 'MacOS, Linux & Windows Subsystem for Linux (WSL)',
+    platforms = 'MacOS, Linux, Windows Subsystem for Linux (WSL)',
 
     packages = find_packages(),
     include_package_data=True,
